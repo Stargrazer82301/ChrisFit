dcbf46dd0c82d4a19d3dd7e5e03ac0eb5116fdeb
Chris Clark <cjrc88@gmail.com>
2017-11-16 17:20:22 +0000

    Created ChrisFit.ModelFlux function, that calculates flux from model
    Supports arbitary number of dust components, potentially each with different kappa and beta, with predictions able to be made in an arbitrary number of bands.

--- ChrisFit.py
+++ ChrisFit.py
@@ -37,3 +37,93 @@ def fit(gal_dict,
 
 
 
+
+def ModelFlux(wavelength, temp, mass, dist, kappa_0=0.051, lambda_0=500E-6, beta=2.0):
+    """
+    Function to caculate flux at given wavelength(s) from dust component(s) of given mass and temperature, at a given distance, assuming modified blackbody ('greybody') emission
+
+    Arguments:
+        wavelength:     A float, or list of floats, giving the wavelength(s) (in m) of interest
+        temp:           A float, or list of floats, giving the temperature(s) (in K) of each dust component
+        mass:           A float, or list of floats, giving the mass(es) (in M_sol) of each dust component
+        dist:           A float, giving the distance to the target source (in pc)
+
+    Keyword arguments:
+        kappa_0:        A float, or list of floats, giving the dust mass absorption coefficient(s) (in m**2 kg**-1), kappa, of each dust component; reference wavelengths given by kwarg lambda_0
+        lambda_0:       A float, or list of floats, giving the reference wavelength (in m) coresponding to each value of kappa_0
+        beta:           A float, or list of floats, giving the dust emissivity slope(s), beta, of each dust component
+
+    If wavelenghth is given as a list, a list of output fluxes will be given, corresponding to the calculated flux at each wavelength.
+
+    Temperature and mass can be set to be lists , corresponding to multiple dust components. For n components, both lists must be of length n.
+
+    Optionally, a different dust mass absorption coefficient can be used for each component; this is done by giving lists of length n for kappa_0 and lambda_0.
+
+    Optionally, a different dust emissivity slope can be used for each component; this is done by giving a list of length n for beta.
+    """
+
+    # Define function for checking if variable is a list, and (if necessary) converting to a n_target length list of identical entries
+    def Numpify(var, n_target=False):
+        if not hasattr(var, '__iter__'):
+            if not n_target:
+                var = list(var)
+            else:
+                var = [var]*n_target
+        else:
+            if len(var)==1 and n_target>1:
+                var = [var[0]]*n_target
+        if not isinstance(var, np.ndarray):
+            var = np.array(var)
+        return var
+
+    # Record number of model components, and number of bands of interest
+    n_comp = len(temp)
+    n_band = len(wavelength)
+
+    # As needed, convert variables to arrays
+    wavelength = Numpify(wavelength)
+    temp = Numpify(temp)
+    mass = Numpify(mass, n_target=n_comp)
+    kappa_0 = Numpify(kappa_0, n_target=n_comp)
+    lambda_0 = Numpify(lambda_0, n_target=n_comp)
+    beta = Numpify(beta, n_target=n_comp)
+
+    # Check that variables are the same length, when they need to be
+    if np.std([len(temp), len(mass), len(beta), len(kappa_0), len(lambda_0)]) != 0:
+        Exception('Number of dust components needs to be identical for temp/mass/beta/kappa_0/lambda_0 variables')
+
+    """ NB: Arrays have dimensons of n_comp rows by n_bands columns """
+
+    # Convert wavelengths to frequencies (for bands of interest, and for kappa_0 reference wavelengths)
+    nu = np.divide(c, wavelength)
+    nu_0 = np.divide(c, lambda_0)
+
+    # Calculate kappa for the frequency of each band of interest
+    kappa_nu_base = np.outer(nu_0**-1, nu) # This being the array-wise equivalent of nu/nu_0
+    kappa_nu_prefactor = np.array([ np.power(kappa_nu_base[m,:],beta[m]) for m in range(n_comp) ]) # This expontiates each model component's base term to its corresponding beta
+    kappa_nu = np.array([ np.multiply(kappa_0[m],kappa_nu_prefactor[m,:]) for m in range(n_comp) ])
+
+    # Caclulate Planck function prefactor for each frequency
+    B_prefactor = np.divide((2.0 * h * nu**3.0), c**2.0)
+
+    # Calculate exponent term in Planck function, for each component, at each frequency
+    B_exponent = np.array([ np.divide((h*nu),(k*temp[m])) for m in range(n_comp) ])
+
+    # Calculate final value of Planck function for each, for each model component, at each frequency (output array will have n_comp rows, and n_freq columns)
+    B_planck = B_prefactor * (np.e**B_exponent - 1)**-1.0
+
+    # Convert mass and distance values to SI unuts
+    mass_kilograms = mass * 2E30
+    dist_metres = dist * 3.26 * 9.5E15
+
+    # Calculate flux for each component, for each dust model component
+    flux = np.zeros(n_band)
+    for m in range(n_comp):
+        flux += 1E26 * kappa_nu[m,:] * dist_metres**-2.0 * mass_kilograms[m] * B_planck[m,:]
+    #flux = ( 1E26 * kappa_nu * dist_metres**-2.0 * mass_kilograms * B_planck )
+
+    return flux
+
+
+
+flux([250E-6,350E-6,500E-6], [15.0,25.0], [1E8,1E5], 25E6, kappa_0=[0.051,0.077], lambda_0=[500E-6,850E-6], beta=[1.5,2.0])
