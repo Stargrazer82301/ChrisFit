2cc9cc693b6a5c13f05d86e8fa8c34ed99d52d12
Chris Clark <cjrc88@gmail.com>
2018-01-18 15:33:53 +0000

    Changed ColourCorrect to be general, no longer requiring dataframe input

--- ChrisFit.py
+++ ChrisFit.py
@@ -100,7 +100,7 @@ def Fit(gal_dict,
                                            kappa_0=kappa_0, kappa_0_lambda=kappa_0_lambda, beta=beta_vector)
 
                 # Update predicted flux value, to factor in colour correction (do this before correlated uncertainties, as colour corrections are calibrated assuming Neptune model is correct)
-                col_correct_factor = ColourCorrect(band_flux_pred, bands_frame.loc[b], temp_vector, mass_vector,
+                col_correct_factor = ColourCorrect(bands_frame.loc[b,'wavelength'], band_frame[b,'band'].split('_')[0], temp_vector, mass_vector,
                                                    kappa_0, kappa_0_lambda, beta, verbose=verbose)
                 band_flux_pred *= col_correct_factor[0]
 
@@ -436,24 +436,23 @@ def LikeBounds(params, fit_dict):
 
 
 
-def ColourCorrect(band_flux_pred, band_frame, temp, mass, kappa_0, kappa_0_lambda, beta, verbose):
+def ColourCorrect(wavelength, instrument, temp, mass, kappa_0, kappa_0_lambda, beta, verbose):
     """ Function to calculate colour-correction FACTOR appropriate to a given underlying spectrum. Will work for any
     instrument for which file 'Color_Corrections_INSTRUMENTNAME.csv' is found in the same directory as this script. """
 
-    # Set location of ChrisFuncs.py to be current working directory, recording the old CWD toswitch back to later
+    # Set location of ChrisFuncs.py to be current working directory, recording the old CWD to switch back to later
     old_cwd = os.getcwd()
     os.chdir(str(os.path.dirname(os.path.realpath(sys.argv[0]))))
 
     # Identify instrument and wavelength, and read in corresponding colour-correction data
     unknown = False
-    instrument = band_frame['band'].split('_')[0]
     try:
         try:
             data_table = np.genfromtxt('Colour_Corrections_'+instrument+'.csv', delimiter=',', names=True)
         except:
             data_table = np.genfromtxt(os.path.join('ChrisFit','Colour_Corrections_'+instrument+'.csv'), delimiter=',', names=True)
         data_index = data_table['alpha']
-        data_column = 'K'+str(int((band_frame['wavelength']*1E6)))
+        data_column = 'K'+str(int(wavelength*1E6))
         data_factor = data_table[data_column]
     except:
         unknown = True
@@ -461,15 +460,15 @@ def ColourCorrect(band_flux_pred, band_frame, temp, mass, kappa_0, kappa_0_lambd
             print(' ')
             print('Instrument \''+instrument+'\' not recognised, no colour correction applied.')
 
-    # If instrument successfuly identified, perform colour correction; otherwise, cease
+    # If instrument successfully identified, perform colour correction; otherwise, cease
     if unknown==True:
         factor = 1.0
         index = np.NaN
     elif unknown==False:
 
         # Calculate relative flux at wavelengths at points at wavelengths 1% to either side of target wavelength (no need for distance or kappa, as absolute value is irrelevant)
-        lambda_plus = band_frame['wavelength']*1.01
-        lambda_minus = band_frame['wavelength']*0.99
+        lambda_plus = wavelength*1.01
+        lambda_minus = wavelength*0.99
         flux_plus = ModelFlux(lambda_plus, temp, mass, 1E6, kappa_0=kappa_0, kappa_0_lambda=kappa_0_lambda, beta=beta)
         flux_minus = ModelFlux(lambda_minus, temp, mass, 1E6, kappa_0=kappa_0, kappa_0_lambda=kappa_0_lambda, beta=beta)
 
