8fb2712860af02134b4dd35c3101f776031eb3b4
Chris Clark <cjrc88@gmail.com>
2021-06-21 16:53:54 -0400

    Created whole separate module, ChrisFitBEMBB, to fit BEMBB SEDs

--- Test/Example_Wrapper_BEMBB.py
+++ Test/Example_Wrapper_BEMBB.py
@@ -12,7 +12,7 @@ import inspect
 import matplotlib
 matplotlib.use('Agg')
 sys.path.append(os.path.dirname(os.path.realpath(os.path.dirname(inspect.getfile(inspect.currentframe())))))
-import ChrisFit
+import ChrisFitBEMBB
 
 
 
@@ -24,9 +24,9 @@ gal_dict = {'name':'Test_Pixel_In_NGC628',
 # Create dataframe storing basic band information
 bands_frame = pd.DataFrame({'band':         ['WISE_22','PACS_70','PACS_100','PACS_160','SPIRE_250','SPIRE_350','SPIRE_500'],
                             'wavelength':   np.array([22E-6, 70E-6, 100E-6, 160E-6, 250E-6, 350E-6, 500E-6,]),
-                            'flux':         np.array([1.319E-3, 0.01478, 0.02406, 0.03998, 0.02324, 0.01153, 0.004160]),
+                            'flux':         np.array([1.319E-3, 0.01478, 0.02406, 0.03998, 0.02324, 0.01153, 1.4*0.004160]),
                             'error':        np.array([1.042E-4, 0.01129, 0.007339, 0.004859, 0.002094, 0.001200, 0.00051224]),
-                            'limit':        [True, False, False, False, False, False, False]})
+                            'limit':        [True, True, True, False, False, False, False]})
 
 # Construct function for SPIRE correlated uncertainty
 def SpireCorrelUnc(prop, unc=0.04):
@@ -39,27 +39,26 @@ def SpireCorrelUnc(prop, unc=0.04):
         y = scipy.ndimage.filters.gaussian_filter1d(y, sigma=len(x)*(0.005/(x.max()-x.min())))
         return y[(np.abs(x-prop)).argmin()]
 
-# Add correlated uncertainty information to band dataframe
+# Store correlated uncertainty information
 correl_unc = [{'correl_bands':['SPIRE_250','SPIRE_350','SPIRE_500'],
                'correl_scale':0.04,
                'correl_distr':SpireCorrelUnc}]
 
 # State output directory
-out_dir = 'Output/'
+out_dir = 'Output_BEMBB/'
 
 # Call ChrisFit
-posterior = ChrisFit.Fit(gal_dict,
-                         bands_frame,
-                         correl_unc = correl_unc,
-                         beta_vary = True,
-                         beta = 2.0,
-                         components = 2,
-                         kappa_0 = 0.051,
-                         kappa_0_lambda = 500E-6,
-                         mcmc_n_threads = int(round(mp.cpu_count()*1.0)),
-                         mcmc_n_walkers = 100,
-                         mcmc_n_steps = 500,
-                         plot = out_dir,
-                         simple_clean = 0.5,
-                         mle_only = False)
+posterior = ChrisFitBEMBB.FitBEMBB(gal_dict,
+                                   bands_frame,
+                                   correl_unc = correl_unc,
+                                   beta = (2.0, 1.5),
+                                   break_lambda = 225E-6,
+                                   kappa_0 = 0.051,
+                                   kappa_0_lambda = 500E-6,
+                                   mcmc_n_threads = int(round(mp.cpu_count()*1.0)),
+                                   mcmc_n_walkers = 250,#250,
+                                   mcmc_n_steps = 500,#500,
+                                   plot = out_dir,
+                                   simple_clean = 0.50,
+                                   map_only = False)
 
